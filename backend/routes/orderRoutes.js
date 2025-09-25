f98c257a1ba606a922270948cb7dee921905e471
RtKush <kush22aur@gmail.com>
2025-09-25 19:29:16 +0530

    implement restock sweet for admin users

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -14,7 +14,40 @@ import { param, check } from 'express-validator';
 const router = express.Router();
 
 const validator = {
- 
+  getOrderById: [
+    param('id').notEmpty().withMessage('Id is required').isMongoId().withMessage('Invalid Id Format')
+  ],
+  updateOrderToPaid: [
+    param('id').notEmpty().withMessage('Id is required').isMongoId().withMessage('Invalid Id Format')
+  ],
+  updateOrderToDeliver: [
+    param('id').notEmpty().withMessage('Id is required').isMongoId().withMessage('Invalid Id Format')
+  ],
+  addOrderItems: [
+    check('cartItems').notEmpty().withMessage('Cart items are required'),
+    check('shippingAddress').notEmpty().withMessage('Shipping address is required'),
+    check('paymentMethod').notEmpty().withMessage('Payment method is required'),
+    check('itemsPrice')
+      .notEmpty()
+      .withMessage('Items price is required')
+      .isNumeric()
+      .withMessage('Items price must be a number'),
+    check('taxPrice')
+      .notEmpty()
+      .withMessage('Tax price is required')
+      .isNumeric()
+      .withMessage('Tax price must be a number'),
+    check('shippingPrice')
+      .notEmpty()
+      .withMessage('Shipping price is required')
+      .isNumeric()
+      .withMessage('Shipping price must be a number'),
+    check('totalPrice')
+      .notEmpty()
+      .withMessage('Total price is required')
+      .isNumeric()
+      .withMessage('Total price must be a number')
+  ]
 }
 
 router.route('/')
