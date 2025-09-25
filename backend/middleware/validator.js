9d6b5db585dda110fa60e01270be4792eb0983ea
RtKush <kush22aur@gmail.com>
2025-09-25 13:01:05 +0530

    refactor(auth): extract user schema validations

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -4,7 +4,7 @@ const validator = (req, res, next) => {
     const result = validationResult(req);
     if(result.isEmpty())
        return next();
-    res.status(400).json({ errors: result.array() });
+    // res.status(400).json({ errors: result.array() });
 }
 
 export default validator;
