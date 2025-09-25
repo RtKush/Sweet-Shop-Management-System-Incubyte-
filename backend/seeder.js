b5274a06714ac2b8166eda29e9baca8dd532b65b
RtKush <kush22aur@gmail.com>
2025-09-25 19:30:26 +0530

    improve error handling with custom error middleware

--- backend/seeder.js
+++ backend/seeder.js
@@ -10,42 +10,9 @@ import 'dotenv/config';
 
 connectDB();
 
-const importData = async () => {
-  try {
-    await Order.deleteMany();
-    await User.deleteMany();
-    await Product.deleteMany();
 
-    const createUsers = await User.insertMany(users);
 
-    const adminUser = createUsers[0]._id;
 
-    const sampleProducts = products.map(product => {
-      return { ...product, user: adminUser };
-    });
-
-    await Product.insertMany(sampleProducts);
-    console.log('Data Imported!'.green.inverse);
-    process.exit();
-  } catch (error) {
-    console.log(`Error: ${error.message}`.red.inverse);
-    process.exit(1);
-  }
-};
-
-const destroyData = async () => {
-  try {
-    await Order.deleteMany();
-    await User.deleteMany();
-    await Product.deleteMany();
-
-    console.log('Data Destroyed!'.red.inverse);
-    process.exit();
-  } catch (error) {
-    console.log(`Error: ${error.message}`.red.inverse);
-    process.exit(1);
-  }
-};
 
 if (process.argv[2] === '-d') {
   destroyData();
